6bc2075f57d264abe92de7ab3c251a5ba2155858
cjasperp <114974914+cjasperp@users.noreply.github.com>
2024-04-02 18:15:17 +0800

    Add files via upload

---
+++ Output.js
@@ -0,0 +1,15 @@
+let bigbang = [] 
+
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 == 0 && i % 5 == 0){
+        bigbang.push("BIGBANG")
+    } else if (i % 3 == 0){
+        bigbang.push("BIG")
+    } else if (i % 5 == 0){
+        bigbang.push("BANG")
+    } else {
+        bigbang.push(i)
+    }
+}
+
+console.log(bigbang)
